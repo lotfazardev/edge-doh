dd13db51783a02097d1b8b8bb75dd2c440a34b08
Mohammad H. Lotfazar <developer.lotfazar@gmail.com>
2025-01-22 20:30:05 +0330

    feat: add ToastContainer configs

--- src/layouts/AppLayout/AppLayout.tsx
+++ src/layouts/AppLayout/AppLayout.tsx
@@ -3,11 +3,21 @@ import BlankLayout from '../BlankLayout';
 import { AppLayoutProps } from './@types';
 import Container from '@src/components/Container';
 import cn from '@src/utils/helper/cn.helper';
+import { ToastContainer } from 'react-toastify';
 
 function AppLayout(props: AppLayoutProps) {
   const { children, className, ...rest } = props;
   return (
     <BlankLayout className={cn('relative bg-[#000005]', className)} {...rest}>
+      <ToastContainer
+        className={'px-6 py-2'}
+        toastClassName={'glass mt-2 rounded-lg'}
+        position="top-center"
+        theme="dark"
+        autoClose={3000}
+        draggable={true}
+        hideProgressBar
+      />
       <Container className="z-10 flex items-center justify-center">
         {children}
       </Container>
